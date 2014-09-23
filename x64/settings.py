1b94b1cc9439c57cda65fc596b65e205009876b4
baiyideng <baiyideng@outlook.com>
2014-09-23 11:16:36 +0800

    build
    发布编译好的x86和x64版本的exe

---
+++ x64/settings.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# IP for the router
+ip = '192.168.1.1'
+# 路由器登录密码
+login_pw = '123'
+
+#PPPOE
+#宽带账号，CD028XXXXX@96301
+pppoe_usename='CD028LKF11@96301'
+pppoe_password='1111'
+
+#wifi password
+wifi_psw=''#不少于8位数
